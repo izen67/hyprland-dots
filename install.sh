94be39b41ab18d600ff63845a045f058f05987da
izen67 <17254913+izen67@users.noreply.github.com>
2025-08-17 18:16:08 +0300

    fix

--- install.sh
+++ install.sh
@@ -49,22 +49,22 @@ if [[ "$ans_audio" =~ ^[Yy]$ ]]; then
     echo "Audio power saving disabled."
   else
     echo "Audio crackle fix already applied."
+  fi
 fi
-fi
-
 
 # Mount SSD (optional)
 read -rp "Do you want to add the SSD mount to /etc/fstab? (y/n): " answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
-    sudo mkdir -p /mnt/extra
-    UUID_LINE='UUID=60fc72ce-8793-4f92-8641-0db9411d931e /mnt/extra ext4 defaults 0 0'
-    if ! grep -Fxq "$UUID_LINE" /etc/fstab; then
-        echo "$UUID_LINE" | sudo tee -a /etc/fstab
-        echo "Mount entry added."
-    else
-        echo "Mount entry already exists."
+  sudo mkdir -p /mnt/extra
+  UUID_LINE='UUID=60fc72ce-8793-4f92-8641-0db9411d931e /mnt/extra ext4 defaults 0 0'
+  if ! grep -Fxq "$UUID_LINE" /etc/fstab; then
+    echo "$UUID_LINE" | sudo tee -a /etc/fstab
+    echo "Mount entry added."
+  else
+    echo "Mount entry already exists."
+  fi
 fi
-fi
+
 
 flatpak install --assumeyes flathub com.github.tchx84.Flatseal
 flatpak install --assumeyes flathub com.github.iwalton3.jellyfin-media-player
